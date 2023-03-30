9875c739ae437451a567db6c5d2a55410d32ff93
Dheeraj Ravipati <dheerajravipati@gmail.com>
2023-03-30 15:10:39 -0500

    project3

---
+++ SparkMatMul/MatMul_dheeraj.scala
@@ -0,0 +1,29 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
+object Multiply {
+
+  def main(args: Array[String]) = {
+    val conf = new SparkConf().setAppName("Multiply")
+    val sc = new SparkContext(conf)
+
+    conf.set("spark.logConf", "false")
+    conf.set("spark.eventLog.enabled", "false")
+
+    val M = sc.textFile(args(0)).map( line => { val a = line.split(",")
+                                      (a(0), (a(1), a(2).toDouble)) } )
+
+    val N = sc.textFile(args(1)).map( line => { val a = line.split(",")
+                                                (a(0), (a(1), a(2).toDouble)) } )
+
+
+    val Mult = M.map { case (i,(j,v)) => ( j, ( i, v ) ) }.join( N.map { case (j,(k,w)) => (j,(k,w)) } )
+                    .map { case (j,((i,v), (k,w))) => ((i,k), v * w) }
+                    .reduceByKey(_+_)
+
+    Mult.sortByKey( true ).coalesce(1, true).saveAsTextFile( args( 2 ) )
+
+    sc.stop()
+
+  }
+}
